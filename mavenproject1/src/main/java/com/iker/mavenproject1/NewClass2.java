6559e59134622fb219649e2bd9025f5e57cd6e12
Ikers890 <ikercuadruizz@gmail.com>
2022-05-27 10:10:02 +0200

    Implementacion JUnit

---
+++ mavenproject1/src/main/java/com/iker/mavenproject1/NewClass2.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.iker.mavenproject1;
+
+/**
+ *
+ * @author windows10
+ */
+public class NewClass2 {
+    
+}
